a0906fa34ecbeb533a2045252f2dda0ae2a91cf8
antonioliverjr <antoniobatistajr@gmail.com>
2021-11-15 22:28:52 -0300

    Finalmente documentando API Flask com Flask-ApiSpec

--- Flask_DIO/app.py
+++ Flask_DIO/app.py
@@ -1,13 +1,32 @@
 from flask import Flask
 from flask_restful import Api
-from habilidades import ListaHabilidades ,Habilidades
+from habilidades import ListaHabilidades, Habilidades
 from desenvolvedores import ListaDesenvolvedores, Desenvolvedor
+
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
 
 app = Flask(__name__)
 api = Api(app)
 
+app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='Desenvolvedores API',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger-json/',
+    'APISPEC_SWAGGER_UI_URL': '/swagger/'
+})
+
+docs = FlaskApiSpec(app)
+
 api.add_resource(ListaDesenvolvedores, '/dev')
+docs.register(ListaDesenvolvedores)
 api.add_resource(Desenvolvedor, '/dev/<int:id>')
+docs.register(Desenvolvedor)
 api.add_resource(Habilidades,  '/habilidades/<int:id>')
 api.add_resource(ListaHabilidades, '/habilidades')
 
--- Flask_DIO/desenvolvedores.py
+++ Flask_DIO/desenvolvedores.py
@@ -1,5 +1,8 @@
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
+from flask_apispec import marshal_with, doc, use_kwargs
+from flask_apispec.views import MethodResource
+from marshmallow import Schema, fields
 import json
 
 desenvolvedores = [
@@ -14,8 +17,8 @@ desenvolvedores = [
     }
 ]
 
-
-class Desenvolvedor(Resource):
+@doc(tags=['Desenvolvedores Atualizações'])
+class Desenvolvedor(MethodResource, Resource):
     def get(self, id):
         try:
             desenvolvedor = desenvolvedores[id-1]
@@ -54,10 +57,32 @@ class Desenvolvedor(Resource):
             response = {'status': 'erro', 'mensagem': messagem}
         return response
 
-class ListaDesenvolvedores(Resource):
-    def post(self):
+class DesenvolvedoresResponseSchema(Schema):
+    message = fields.Str(default='Sucess')
+class DesenvolvedoresRequestSchema(Schema):
+    nome = fields.String(required=True, description='Informe o nome do desenvolvedor')
+    habilidades = fields.List(fields.String(description='Informa habilidade do desenvolvedor'))
+class DesenvolvedoresRequestFailSchema(Schema):
+    message = fields.Str(default='Error')
+
+@doc(tags=['Desenvolvedores'])
+class ListaDesenvolvedores(MethodResource, Resource):
+    @doc(description='Adicione um desenvolvedor')
+    @use_kwargs(DesenvolvedoresRequestSchema, location='json')
+    @marshal_with(DesenvolvedoresResponseSchema, code=200, description='Cadastrado com sucesso.')
+    @marshal_with(DesenvolvedoresRequestFailSchema, description='Erro ao cadastrar', code=400)
+    @marshal_with(DesenvolvedoresRequestFailSchema, description='Campos Nulos', code=422)
+    def post(self, **kwargs):
         try:
             dados = json.loads(request.data)
+            if(dados is None):
+                response = make_response('', 422)
+                return response
+
+            if(dados['nome'] is None or dados['nome'] == ''):
+                response = make_response('', 400)
+                return response
+
             posicao = len(desenvolvedores)
             dados['id'] = int(posicao) + 1
             desenvolvedores.append(dados)
@@ -66,5 +91,7 @@ class ListaDesenvolvedores(Resource):
             messagem = 'Erro desconhecido. Procure o administrador da API'
             response = {'status':'error', 'mensagem':messagem}
             return response
+    @doc(description='Lista Desenvolvedores')
+    #@marshal_with(DesenvolvedoresResponseSchema)
     def get(self):
         return desenvolvedores
