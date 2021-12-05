212ade616144e63bf58b1df4e1fc33b54b571b85
antonioliverjr <antoniobatistajr@gmail.com>
2021-12-05 18:58:36 -0300

    Gerador de CPF

--- Udemy_CursoPython/AtividadesAulas/DesafioSecao2/geradorCPF.py
+++ Udemy_CursoPython/AtividadesAulas/DesafioSecao2/geradorCPF.py
@@ -1,36 +1,31 @@
 """
 CPF = ***.***.***-**
 """
-print('{:#^50}'.format(''))
-print('{:#^50}'.format(' VÁLIDADOR DE CPF '))
-print('{:#^50}'.format(''))
-while True:
-    cpf = input("Informe o CPF para validação: ")
-    if len(cpf) != 11:
-        continue
-    else:
-        break
+import random
 
-cpf = cpf.replace('.', '').replace('-', '').replace(',', '')
-valida_cpf = cpf[0:9]
+print('{:#^50}'.format(''))
+print('{:#^50}'.format(' GERADOR DE CPF '))
+print('{:#^50}'.format(''))
+
+cpf = str(random.randint(100000000, 999999999))
 ###################################################
 soma_cpf = 0
 for indice, mult in enumerate(range(10, 1, -1)):
-    soma_cpf += int(valida_cpf[indice]) * mult
+    soma_cpf += int(cpf[indice]) * mult
 
 formula1 = 11 - (soma_cpf % 11)
 digito1 = '0' if (formula1 > 9) else str(formula1)
 
-valida_cpf += digito1
+cpf += digito1
 ###################################################
 soma_cpf = 0
 for indice, mult in enumerate(range(11, 1, -1)):
-    soma_cpf += int(valida_cpf[indice]) * mult
+    soma_cpf += int(cpf[indice]) * mult
 
 formula2 = 11 - (soma_cpf % 11)
 digito2 = '0' if (formula2 > 9) else str(formula2)
 
-valida_cpf += digito2
+cpf += digito2
 ##################################################
-print('{:#^50}'.format(' CPF Válido ' if cpf == valida_cpf else ' CPF inválido! '))
+print('{:#^50}'.format(' CPF Válido {}.{}.{}-{} '.format(cpf[0:3], cpf[3:6], cpf[6:9], cpf[9:11])))
 print('{:#^50}'.format(''))
