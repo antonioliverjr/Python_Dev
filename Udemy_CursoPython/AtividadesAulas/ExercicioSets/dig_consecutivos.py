f3eb1f3d6515efc3e79fc7458e0056aca59c80ea
antonioliverjr <antoniobatistajr@gmail.com>
2021-12-11 21:44:44 -0300

    Exercitando problema do grupo pycode, verificar sequencia de numero de 0 a 9

---
+++ Udemy_CursoPython/AtividadesAulas/ExercicioSets/dig_consecutivos.py
@@ -0,0 +1,49 @@
+sequencia1 = 53590
+# saída = 90
+sequencia2 = 67403098567819
+# saída = 5678
+sequencia3 = 9012364509789
+# saída = 90123
+
+
+def verificaMaior(lista):
+    tam = 0
+    retorno = []
+    for valor in lista:
+        if len(valor) > tam:
+            tam = len(valor)
+            retorno = [x for x in valor]
+    return retorno
+
+
+def ver_sequencia(lista):
+    stg_seq = []
+    sequencia = []
+    listagem = [x for x in str(lista)]
+    for indice, valor in enumerate(listagem):
+        if indice < (len(listagem)-1):
+            if int(valor)+1 == int(listagem[indice+1]):
+                if valor in sequencia:
+                    sequencia.append(listagem[indice + 1])
+                else:
+                    sequencia.append(valor)
+                    sequencia.append(listagem[indice+1])
+            elif int(valor) == 9 and int(listagem[indice+1]) == 0:
+                if valor in sequencia:
+                    sequencia.append(listagem[indice + 1])
+                else:
+                    sequencia.append(valor)
+                    sequencia.append(listagem[indice+1])
+            else:
+                if len(sequencia) > 0:
+                    stg_seq.append([x for x in sequencia])
+                    sequencia.clear()
+    if len(stg_seq) > 0:
+        sequencia = verificaMaior(stg_seq)
+    sequencia_result = ''.join([x for x in sequencia])
+    return sequencia_result
+
+
+print(ver_sequencia(sequencia1))
+print(ver_sequencia(sequencia2))
+print(ver_sequencia(sequencia3))
