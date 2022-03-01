6084befe514a36e3fa9f3a076f87648bfb4d5690
antonioliverjr <antoniobatistajr@gmail.com>
2022-03-01 20:29:20 -0300

    Testando package viacep

--- Flask_DIO/FastAPI/main.py
+++ Flask_DIO/FastAPI/main.py
@@ -1,11 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
+from package_viacep import viacep
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"Message": "Hello Word!"}
+    casa = viacep.ViaCep()
+    return casa.GetData('42808193')
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
